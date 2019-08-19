f822f231447797735f04bcb37a5a4573a844cc70
Mikhail Girkin <mikegirkin@gmail.com>
2019-08-19 18:23:14 +0100

    Remove shite

--- src/main/scala/net/girkin/gomoku/HealthCheckService.scala
+++ src/main/scala/net/girkin/gomoku/HealthCheckService.scala
@@ -2,15 +2,15 @@ package net.girkin.gomoku
 
 import cats.effect.Effect
 import io.circe.Json
-import org.http4s.HttpService
+import org.http4s.HttpRoutes
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 
 
 class HealthCheckService[F[_]: Effect] extends Http4sDsl[F] {
 
-  val service: HttpService[F] = {
-    HttpService[F] {
+  val service: HttpRoutes[F] = {
+    HttpRoutes.of[F] {
       case GET -> Root / "_internal_" / "healthcheck" =>
         Ok(Json.obj("status" -> Json.fromString("ok")))
     }
